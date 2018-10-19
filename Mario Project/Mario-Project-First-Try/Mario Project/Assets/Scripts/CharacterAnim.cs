e535f184ee9492f70f99794a4a4a12c36dd0aa9b
Cruz441 <Cruz441@users.noreply.github.com>
2018-10-18 23:20:03 -0400

    e

---
+++ Mario Project/Mario-Project-First-Try/Mario Project/Assets/Scripts/CharacterAnim.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("isRunningRight", true);
+        }
+        else
+        {
+            anim.SetBool("isRunningRight", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            anim.SetBool("isRunningLeft", true);
+        }
+        else
+        {
+            anim.SetBool("isRunningLeft", false);
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.SetTrigger("isJumping");
+        }
+	}
+}
