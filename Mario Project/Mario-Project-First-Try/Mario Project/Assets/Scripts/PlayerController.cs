a9f5bb46fa6244cada7be3c61df64956677ceaaf
Cruz441 <Cruz441@users.noreply.github.com>
2018-10-19 12:15:44 -0400

    coin box start

--- Mario Project/Mario-Project-First-Try/Mario Project/Assets/Scripts/PlayerController.cs
+++ Mario Project/Mario-Project-First-Try/Mario Project/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public float jumpForce;
+    public AudioSource coinPickup;
+    public Text countText;
 
     private Rigidbody2D rb2d;
-   
+    private int count;
+
     // Use this for initialization
     void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        coinPickup = GetComponent<AudioSource>();
+        count = 0;
+        SetCountText();
 	}
 	
     void FixedUpdate()
@@ -20,13 +29,14 @@ public class PlayerController : MonoBehaviour {
         rb2d.AddForce(movement * speed);
     }
 
+
     private void OnCollisionStay2D(Collision2D collision)
     {
         if(collision.collider.tag == "Ground")
         {
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                rb2d.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             }
         }
     }
@@ -36,6 +46,14 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("Pickup"))
         {
             other.gameObject.SetActive(false);
+            coinPickup.Play();
+            count = count + 1;
+            SetCountText();
         }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
     }
 }
